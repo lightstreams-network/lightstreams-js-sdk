472c20de5f53d8eda11411721e959dbfab92bc39
ggarri <gabriel@lightstreams.io>
2019-05-15 18:41:01 +0200

    Passing through entire response in case of fetch streaming

--- src/lib/request.js
+++ src/lib/request.js
@@ -66,7 +66,7 @@ module.exports = (() => {
       method: 'GET',
     }).then((res) => {
       if(res.status === 200 && options['stream']) {
-        return res.body;
+        return res;
       }
       return parseResponse(res)
     });
