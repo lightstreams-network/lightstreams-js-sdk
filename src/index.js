64f11aa9d81a0648ae7917dcbdcf6d1ce41c4739
ggarri <gabriel@lightstreams.io>
2019-09-10 13:29:56 +0200

    Remove lightwallet usage due to package version incompatibilities

--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@
 
 // Fix Error: More than one instance of bitcore-lib found. Please make sure to require bitcore-lib
 // and check that submodules do not also include their own bitcore-lib dependency.
-Object.defineProperty(global, '_bitcore', {
-  get() {
-    return undefined
-  }, set() {
-  }
-});
+// Object.defineProperty(global, '_bitcore', {
+//   get() {
+//     return undefined
+//   }, set() {
+//   }
+// });
 
 module.exports.Gateway = (gwDomain) => ({
   user: require('./gateway/user')(gwDomain),
@@ -18,11 +18,6 @@ module.exports.Gateway = (gwDomain) => ({
   erc20: require('./gateway/erc20')(gwDomain),
 });
 
-module.exports.Lightwallet = {
-  Keystore: require('./lightwallet/keystore'),
-  Signing: require('./lightwallet/signing'),
-  Web3Provider: require('./lightwallet/web3provider')
-};
 module.exports.MetaMask = require('./metamask');
 module.exports.Web3 = require('./web3');
 module.exports.Web3Provider = require('./web3-provider');
