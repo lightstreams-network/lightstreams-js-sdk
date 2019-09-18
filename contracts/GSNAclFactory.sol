45a2e0d22049cd086e749fa085a83b3de943ea3e
Lukas Lukac <lukas.lukac.io@gmail.com>
2019-09-18 09:59:20 +0200

    #7 Merges Init and GSNRecipient to one contract: GSN.

--- contracts/GSNAclFactory.sol
+++ contracts/GSNAclFactory.sol
@@ -1,14 +1,13 @@
 pragma solidity ^0.5.0;
 
-import "@openzeppelin/contracts-ethereum-package/contracts/GSN/GSNRecipient.sol";
-import "@openzeppelin/upgrades/contracts/Initializable.sol";
 import "./GSNAcl.sol";
+import "./GSN.sol";
 
 /**
  * @title ACLFactory enables gas-free interactions with ACL from deployment to rights management.
  * @author Lukas Lukac, Lightstreams, 22.8.2018
  */
-contract GSNAclFactory is Initializable, GSNRecipient {
+contract GSNAclFactory is GSN {
     uint256 constant public aclFunding = 10 ether;
 
     address[] acls;
@@ -16,11 +15,6 @@ contract GSNAclFactory is Initializable, GSNRecipient {
     event NewACL(address addr);
 
     function() payable external {}
-
-    function initialize(address relayHub) public initializer {
-        GSNRecipient.initialize();
-        _upgradeRelayHub(relayHub);
-    }
 
     function newACL(address _owner) public returns (address) {
         GSNAcl acl = new GSNAcl(_owner, false);
@@ -32,27 +26,5 @@ contract GSNAclFactory is Initializable, GSNRecipient {
         emit NewACL(addr);
 
         return addr;
-    }
-
-    function acceptRelayedCall(
-        address,
-        address,
-        bytes calldata,
-        uint256,
-        uint256,
-        uint256,
-        uint256,
-        bytes calldata,
-        uint256
-    ) external view returns (uint256, bytes memory) {
-        return _approveRelayedCall();
-    }
-
-    function getRecipientBalance() public view returns (uint) {
-        return IRelayHub(getHubAddr()).balanceOf(address(this));
-    }
-
-    function version() public pure returns (uint256) {
-        return 1;
     }
 }
