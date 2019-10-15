4a22ca20bda521cf67a53c177e0b870fb00bee3b
ggarri <gabriel@lightstreams.io>
2019-10-15 13:10:08 +0200

    Restore missing method from Web3 wrapper

--- src/web3/addon/keystore.js
+++ src/web3/addon/keystore.js
@@ -67,3 +67,12 @@ module.exports.isAccountLocked = (web3, { address }) => {
 
   return web3.currentProvider.isAccountLocked(address);
 };
+
+module.exports.exportMnemonic = (web3, { address }) => {
+  if (typeof web3.currentProvider._getAccount !== 'function') {
+    throw new Error(`Not supported method`)
+  }
+
+  const account = web3.currentProvider._getAccount(address);
+  return account.seedPhrase();
+};
