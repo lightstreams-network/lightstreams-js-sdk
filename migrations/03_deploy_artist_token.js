ea1aa975b9cbb25b733e00b61d6f8a4f8e2edb29
Lukas Lukac <lukas.lukac.io@gmail.com>
2019-10-16 10:54:35 +0200

    Updating master and resolving conflicts.

--- migrations/03_deploy_artist_token.js
+++ migrations/03_deploy_artist_token.js
@@ -6,7 +6,7 @@ const FundingPoolMock = artifacts.require("FundingPoolMock");
 // Curve parameters:
 const reserveRatio = 142857;  // Kappa (~ 6)
 const theta = 350000;         // 35% in ppm
-const p0 = 1;                // Price of internal token in external tokens.
+const p0 = 1;                 // Price of internal token in external tokens.
 const initialRaise = 300000;  // Raise amount in external tokens.
 const friction = 20000;       // 2% in ppm
 const gasPrice = 15000000000; // 15 gwei
@@ -20,9 +20,9 @@ module.exports = function(deployer) {
 
   deployer.then(() => {
     return FundingPoolMock.deployed();
-  }).then((curInstance) => {
-    if (curInstance.address && !global.forceMigration('03')) {
-      console.log(`Contract already deployed ${curInstance.address}. Skipped migration "03_deploy_artist_token.js`);
+  }).then((fundingPoolInstance) => {
+    if (fundingPoolInstance.address && !global.forceMigration('03')) {
+      console.log(`Contract already deployed ${fundingPoolInstance.address}. Skipped migration "03_deploy_artist_token.js`);
       return null;
     }
 
